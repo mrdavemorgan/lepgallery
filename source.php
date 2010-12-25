1146a9756c24becabba97bbf44583afc74700c6c
mmond <mmond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-25 11:01:37 +0000

    Images centered, tagging started.
    git-svn-id: https://plugins.svn.wordpress.org/ungallery/tags/1.3.0@325996 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- source.php
+++ source.php
@@ -8,12 +8,13 @@ if ($_GET['zip']) {
 	$lastslash =  strrpos($filename, "/");
 	$name =  substr($filename, $lastslash + 1);
 	
-	header('Content-type: application/x-zip-compressed');
-	header('Content-Length: $len');
+	header("Content-type: application/x-zip-compressed;\r\n"); 
+	header("Content-Length: $len;\r\n");
 	header('Content-Disposition: attachment; filename="' . $name . '"');  // Create a download stream link
 	readfile($filename);	
 }
 
+/*  Deprecated, handled by phpThumb lib
 if ($_GET['pic']) {
 	$filename = $_GET['pic'];
 	$len = filesize($filename);
@@ -25,7 +26,7 @@ if ($_GET['pic']) {
 	header("Content-Transfer-Encoding: binary;\r\n");
 	header('Content-Disposition: inline; filename="'.$name.'"');	//  Render the photo inline.
 	readfile($filename);
-} 
+} */
 
 if ($_GET['movie']) {
 	$filename = $_GET['movie'];
@@ -33,7 +34,7 @@ if ($_GET['movie']) {
 	$lastslash =  strrpos($filename, "/");
 	$name =  substr($filename, $lastslash + 1);   
 
-	header("Content-type: video/x-msvideo;\r\n");
+	header("Content-type: video/mp4;\r\n");
 	header("Content-Length: $len;\r\n");
 	header("Content-Transfer-Encoding: binary;\r\n");
 	header('Content-Disposition: inline; filename="'.$name.'"');	//  Render the video inline.
