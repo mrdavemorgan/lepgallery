f2b1c77fdc08cd585809537b18df5cb66ee6f32e
mmond <mmond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-30 16:20:23 +0000

    Updated, corrected version.
    git-svn-id: https://plugins.svn.wordpress.org/ungallery/tags/1.4.2@327226 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/source.php
+++ trunk/source.php
@@ -1,24 +1,23 @@
-<?php
+<?
 // 	Choose the URL argument being passed to source.php
 //	Build the content type link based on file type
 
 if ($_GET['zip']) {
-	$filename = str_replace("/", "\\", $_GET['zip']);
+	$filename = $_GET['zip'];
 	$len = filesize($filename);
-	$lastslash =  strrpos($filename, "\\");
+	$lastslash =  strrpos($filename, "/");
 	$name =  substr($filename, $lastslash + 1);
 	
-	header("Content-type: application/x-zip-compressed;\r\n");
+	header("Content-type: application/x-zip-compressed;\r\n"); 
 	header("Content-Length: $len;\r\n");
-	header("Content-Transfer-Encoding: binary;\r\n");
 	header('Content-Disposition: attachment; filename="' . $name . '"');  // Create a download stream link
 	readfile($filename);	
 }
 
 if ($_GET['pic']) {
-	$filename = str_replace("/", "\\", $_GET['pic']);
+	$filename = $_GET['pic'];
 	$len = filesize($filename);
-	$lastslash =  strrpos($filename, "\\");
+	$lastslash =  strrpos($filename, "/");
 	$name =  substr($filename, $lastslash + 1);   
 
 	header("Content-type: image/jpeg;\r\n");
@@ -29,12 +28,12 @@ if ($_GET['pic']) {
 } 
 
 if ($_GET['movie']) {
-	$filename = str_replace("/", "\\", $_GET['movie']);
+	$filename = $_GET['movie'];
 	$len = filesize($filename);
-	$lastslash =  strrpos($filename, "\\");
+	$lastslash =  strrpos($filename, "/");
 	$name =  substr($filename, $lastslash + 1);   
 
-	header("Content-type: video/x-msvideo;\r\n");
+	header("Content-type: video/mp4;\r\n");
 	header("Content-Length: $len;\r\n");
 	header("Content-Transfer-Encoding: binary;\r\n");
 	header('Content-Disposition: inline; filename="'.$name.'"');	//  Render the video inline.
