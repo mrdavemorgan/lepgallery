116bed9745251d592a176a770454696395f40084
mmond <mmond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-19 15:14:34 +0000

    Further limit search string parameters to only common filename characters.
    git-svn-id: https://plugins.svn.wordpress.org/ungallery/trunk@614558 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- search.php
+++ search.php
@@ -19,37 +19,37 @@ $gallerylink = ($_GET['gallerylink']) ;
 $search = ($_GET['search']) ;
 $dir = $pic_root . $gallerylink;
 
-// Do not allow any termination of search parameter
-if (strpos($search, ";")) print "Invalid Search"; 
-	else {
+// Only allow filename characters in search parameter to prevent termination and cmd execution
+$legal = '/^[a-zA-Z0-9_.-]*$/';
+if (preg_match($legal, $search)) {
 
-//	Find galleries
-print "Searching in <i>$dir.</i>
-<br />
-<br />";
+	//	Find galleries
+	print "Searching in <i>$dir.</i>
+	<br />
+	<br />";
 
-print "These galleries matched <i>$search:</i><br />";
-$galleries = `find $dir -iname \*$search\* -type d`;
+	print "These galleries matched <i>$search:</i><br />";
+	$galleries = `find $dir -iname \*$search\* -type d`;
 
-$galleries = explode("\n", $galleries);
+	$galleries = explode("\n", $galleries);
 
-foreach ($galleries as $gallery_path) {
-	$gallery = str_replace($pic_root, "", $gallery_path) ;
-	$link = '<a href="'. $permalink .'?gallerylink=' . $gallery . '">' . $gallery . '</a>';
-	if (!strpos($gallery, $hidden)) print "&nbsp;" . $link . "<br />"; 
-}
+	foreach ($galleries as $gallery_path) {
+		$gallery = str_replace($pic_root, "", $gallery_path) ;
+		$link = '<a href="'. $permalink .'?gallerylink=' . $gallery . '">' . $gallery . '</a>';
+		if (!strpos($gallery, $hidden)) print "&nbsp;" . $link . "<br />"; 
+	}
 
-print "These files matched <i>$search:</i><br />";
-$files = `find $dir -iname \*$search\* -type f`;
+	print "These files matched <i>$search:</i><br />";
+	$files = `find $dir -iname \*$search\* -type f`;
 
-$files = explode("\n", $files);
+	$files = explode("\n", $files);
 
-foreach ($files as $file_path) {
-	$file = str_replace($pic_root, "", $file_path) ;
-	#$link = '<a href="'. $permalink .'?src=' . $file_path  . '">' . $file . '</a>';
-	if (stripos(strrev($file), "4pm") === 0) $link = '<a href="' . $blogURI . 'wp-content/plugins/ungallery/source.php?movie=' . $file_path  . '">' . $file . '</a>';
-	else $link = '<a href="' . $blogURI . 'wp-content/plugins/ungallery/phpthumb/phpThumb.php?src=' . rawurlencode($file_path) .'">' . $file . '</a>'; 
-	if (!strpos($file, $hidden)) print "&nbsp;" . $link . "<br />"; 
-} 
-}
+	foreach ($files as $file_path) {
+		$file = str_replace($pic_root, "", $file_path) ;
+		#$link = '<a href="'. $permalink .'?src=' . $file_path  . '">' . $file . '</a>';
+		if (stripos(strrev($file), "4pm") === 0) $link = '<a href="' . $blogURI . 'wp-content/plugins/ungallery/source.php?movie=' . $file_path  . '">' . $file . '</a>';
+		else $link = '<a href="' . $blogURI . 'wp-content/plugins/ungallery/phpthumb/phpThumb.php?src=' . rawurlencode($file_path) .'">' . $file . '</a>'; 
+		if (!strpos($file, $hidden)) print "&nbsp;" . $link . "<br />"; 
+	} 
+}	else print "Invalid Search";
 ?>
